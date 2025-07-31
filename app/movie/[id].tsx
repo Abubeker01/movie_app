e2761d4b813e79149d5fd892a39eabe8e3f2b43f
abubeker <abubekermurad1@gmail.com>
2025-07-31 08:13:20 +0300

    search

--- app/movie/[id].tsx
+++ app/movie/[id].tsx
@@ -6,20 +6,10 @@ const MovieDetail = () => {
   const { id } = useLocalSearchParams();
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Movie Detail Screen for ID: {id}</Text>
+    <View >
+      <Text >Movie Detail Screen for ID: {id}</Text>
     </View>
   )
 }
 
 export default MovieDetail;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 18,
-}})
